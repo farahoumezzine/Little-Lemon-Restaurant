2005490a1e24eaeb97b29b4c8f7f80ad0ecc35b4
Oumezzine Farah <oumezzine.farah@yahoo.com>
2023-09-07 12:59:49 +0100

    working with apiHTTP Request/Response

--- App.js
+++ App.js
@@ -1,89 +1,80 @@
-import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
 import {
-  StyleSheet,
+  ActivityIndicator,
+  FlatList,
   Text,
-  TextInput,
   View,
-  useColorScheme,
+  StyleSheet,
+  SafeAreaView,
 } from "react-native";
 
-//import local components here
-import LittleLemonHeader from "./components/LittleLemonHeader";
-import * as React from "react";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-import LittleLemonFooter from "./components/LittleLemonFooter";
-import WelcomeScreen from "./components/WelcomeScreen";
-
-import MenuItemsScrollView from "./components/MenuItemsScrollView";
-import MenuItems2 from "./components/MenuItems2.";
-import MenuItems from "./components/MenuItems";
-import Textinput from "./components/Textinput";
-import LoginScreen from "./components/LoginScreen";
-import Imagefront from "./components/Imagefront";
-import Exercice from "./components/Exercice";
-
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-export default function App() {
-  const Stack = createNativeStackNavigator();
-  const MyTheme = {
-    ...DefaultTheme,
-
-    colors: {
-      ...DefaultTheme.colors,
-
-      background: "#495E57",
-    },
+  const getMenu = async () => {
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu-items-by-category.json"
+      );
+      const json = await response.json();
+      setData(json.menu);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
-  return (
-    <NavigationContainer theme={MyTheme}>
-      <View style={styles.container}>
-        <LittleLemonHeader />
 
-        <Stack.Navigator
-          initialRouteName="Welcome"
-          screenOptions={{
-            headerStyle: { backgroundColor: "#FBDABB" },
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
-        >
-          <Stack.Screen
-            name="Welcome"
-            component={WelcomeScreen}
-            options={{ title: "Home", headerTitleAlign: "center" }}
-          />
-          <Stack.Screen name="Menu" component={MenuItems} />
-        </Stack.Navigator>
-      </View>
+  useEffect(() => {
+    getMenu();
+  }, []);
 
-      <View>
-        <LittleLemonFooter />
-      </View>
-    </NavigationContainer>
+  const Item = ({ name, price }) => (
+    <View style={menuStyles.innerContainer}>
+      <Text style={menuStyles.itemText}>{name}</Text>
+      <Text style={menuStyles.itemText}>{"$" + price}</Text>
+    </View>
   );
 
-  {
-    /*    <>
-      <View style={styles.container}>
-        <LittleLemonHeader></LittleLemonHeader>
-        <WelcomeScreen></WelcomeScreen>
-      </View>
+  const renderItem = ({ item }) => (
+    <Item name={item.title} price={item.price} />
+  );
 
-      <View style={styles.container2}>
-        <LittleLemonFooter></LittleLemonFooter>
-      </View>
-    </> */
-  }
-}
-const styles = StyleSheet.create({
+  return (
+    <SafeAreaView style={menuStyles.container}>
+      <Text style={menuStyles.headerText}>Little Lemon</Text>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={renderItem}
+        />
+      )}
+    </SafeAreaView>
+  );
+};
+
+const menuStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#495E57",
   },
-  container2: {
+  innerContainer: {
+    paddingHorizontal: 40,
+    paddingVertical: 20,
     backgroundColor: "#495E57",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  itemText: {
+    color: "#F4CE14",
+    fontSize: 22,
+  },
+  headerText: {
+    color: "#495E57",
+    fontSize: 30,
+    textAlign: "center",
   },
 });
